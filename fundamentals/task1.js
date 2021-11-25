a6e1bcdf0419f916bdc914e66cddd7eccd925ca9
Hanna <northernwind@tut.by>
2021-11-25 14:01:48 +0300

    feat: complete task1

--- fundamentals/task1.js
+++ fundamentals/task1.js
@@ -1,5 +1,10 @@
-// Variables: 
-// Declare admin and name variables. 
-// Assign the value "John" to name. 
-// Copy the value from name to admin. 
+// Variables:
+// Declare admin and name variables.
+// Assign the value "John" to name.
+// Copy the value from name to admin.
 // Show the value of admin using alert (must output “John”).
+
+let admin, name;
+name = "John";
+admin = name;
+alert(admin);
