ca28a0b783fb572a7721b5d7cc6bd2d7521a7659
Hanna <northernwind@tut.by>
2021-11-26 16:27:45 +0300

    feat: solve task8

--- fundamentals/task8.js
+++ fundamentals/task8.js
@@ -2,3 +2,8 @@
 // If the visitor enters another number – ask them to input again.
 // The loop must ask for a number until either the visitor enters a number greater than 100
 // or cancels the input/enters an empty line.
+
+let userInput;
+do {
+  userInput = prompt("Enter a number greater than 100");
+} while (userInput && userInput <= 100);
