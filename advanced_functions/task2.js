f13be6a93bbf8e3a21f0627b042e26f6f822b0a5
Hanna <northernwind@tut.by>
2021-11-26 17:29:12 +0300

    init: create advanced_functions tasks files

---
+++ advanced_functions/task2.js
@@ -0,0 +1,6 @@
+// Write a sum() function which will work properly when invoked using syntax below:
+// ==================================
+// sum(2,3); // Outputs 5
+// sum(2)(3); // Outputs 5
+// sum(1)(2)(3)(4); // Outputs 10
+// ==================================
