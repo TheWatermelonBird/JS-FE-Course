3a44c1470b59470c2bc2fc002ce1927a9bc8e143
Hanna <northernwind@tut.by>
2021-11-26 16:09:44 +0300

    feat: solve taks3

--- fundamentals/task3.js
+++ fundamentals/task3.js
@@ -4,3 +4,13 @@
 // ==================================
 // createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
 // ==================================
+
+function createPhoneNumber(d) {
+  //   ugly code
+  //   return `(${d[0]}${d[1]}${d[2]}) ${d[3]}${d[4]}${d[5]}-${d[6]}${d[7]}${d[8]}${d[9]}`;
+
+  //   regexp code
+  let number = d.join("");
+  let segment = number.match(/^(\d{3})(\d{3})(\d{4})$/);
+  return `(${segment[1]}) ${segment[2]}-${segment[3]}`;
+}
