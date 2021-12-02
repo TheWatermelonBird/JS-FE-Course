7ef2032cdbb90ce29a0cf8a0e7e014e06cefbfae
Hanna <northernwind@tut.by>
2021-12-02 13:57:42 +0300

    feat: update task10

--- fundamentals/task10.js
+++ fundamentals/task10.js
@@ -12,3 +12,8 @@
 function checkAge(age) {
   return age > 18 ? true : confirm("Did parents allow you?");
 }
+
+// Honestly I looked this variant up on javascript.info, will remember bcs is is cool and shorter!
+function checkAgeShorter(age) {
+  return age > 18 || confirm("Did parents allow you?");
+}
